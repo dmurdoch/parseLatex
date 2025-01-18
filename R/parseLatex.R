d6adf2d1b87d9eed9376e3b30d54f4b6c4397c80
Duncan Murdoch <murdoch.duncan@gmail.com>
2025-01-18 09:24:39 -0500

    Initial commit

---
+++ R/parseLatex.R
@@ -0,0 +1,8 @@
+
+parseLatex <- function(text, filename = deparse1(substitute(text)),
+           verbose = FALSE,
+           verbatim = c("verbatim", "verbatim*",
+                        "Sinput", "Soutput"),
+           verb = "\\Sexpr") {
+
+}
